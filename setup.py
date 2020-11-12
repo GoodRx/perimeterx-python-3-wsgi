2a8cc5d469d13b35284a885f57e465a8fc0a572d
Andy Freeland <andy@andyfreeland.net>
2020-11-12 10:25:23 -0800

    Use setuptools_scm so each commit to this branch has a unique version

--- setup.py
+++ setup.py
@@ -2,15 +2,12 @@
 
 from setuptools import setup, find_packages
 
-version = 'v1.1.0'
 setup(name='perimeterx-python-3-wsgi',
-      version=version,
       license='MIT',
       description='PerimeterX Python 3 WSGI middleware',
       author='Johnny Tordgeman',
       author_email='johnny@perimeterx.com',
       url='https://github.com/PerimeterX/perimeterx-python-3-wsgi',
-      download_url='https://github.com/PerimeterX/perimeterx-python-3-wsgi/tarball/' + version,
       packages=find_packages(exclude=['dev', 'test*']),
       package_data={'perimeterx': ['templates/*']},
       install_requires=['pystache>=0.5.4', 'requests>=2.22.0', 'Werkzeug==0.16.0', 'pycryptodome>=3.9.0'],
